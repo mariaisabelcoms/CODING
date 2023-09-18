b86c6353090c8b4132ba64b706594dd7dccc2d40
MARIA ISABEL <mariaisabelcoms@outlook.com>
2023-09-18 17:52:08 -0300

    Histórico de Estudo 18/09

---
+++ testeai.jsx
@@ -0,0 +1,257 @@
+{
+    checklist_title: "a checklist that informs all of the necessary topics to be included in a pharmaceutical package according to Brazilian RDC 768",
+     [
+      {
+        title: "General Information",
+        substeps: [
+          {
+            title: "Company name",
+            details: null
+          },
+          {
+            title: "Product name",
+            details: null
+          },
+          {
+            title: "Package description",
+            details: null
+          },
+          {
+            title: "Package size",
+            details: null
+          },
+          {
+            title: "Batch number",
+            details: null
+          },
+          {
+            title: "Manufacturing date",
+            details: null
+          },
+          {
+            title: "Expiry date",
+            details: null
+          },
+          {
+            title: "Storage conditions",
+            details: null
+          }
+        ]
+      },
+      {
+        title: "Labeling Requirements",
+        substeps: [
+          {
+            title: "Primary and secondary packaging labels",
+            details: null
+          },
+          {
+            title: "Proper language and legibility",
+            details: null
+          },
+          {
+            title: "Name of the product",
+            details: null
+          },
+          {
+            title: "Active ingredient",
+            details: null
+          },
+          {
+            title: "Strength and dosage form",
+            details: null
+          },
+          {
+            title: "Route of administration",
+            details: null
+          },
+          {
+            title: "Pharmaceutical form",
+            details: null
+          },
+          {
+            title: "Therapeutic indications",
+            details: null
+          },
+          {
+            title: "Contraindications",
+            details: null
+          },
+          {
+            title: "Precautions",
+            details: null
+          },
+          {
+            title: "Adverse effects",
+            details: null
+          },
+          {
+            title: "Dosage and administration instructions",
+            details: null
+          },
+          {
+            title: "Special warnings",
+            details: null
+          },
+          {
+            title: "Storage conditions",
+            details: null
+          },
+          {
+            title: "Lot number",
+            details: null
+          },
+          {
+            title: "Expiry date",
+            details: null
+          },
+          {
+            title: "Registration number",
+            details: null
+          },
+          {
+            title: "Barcodes",
+            details: null
+          },
+          {
+            title: "Packaging material details",
+            details: null
+          }
+        ]
+      },
+      {
+        title: "Patient Information",
+        substeps: [
+          {
+            title: "Patient leaflet insert",
+            details: null
+          },
+          {
+            title: "Clear instructions for use",
+            details: null
+          },
+          {
+            title: "Dosage instructions",
+            details: null
+          },
+          {
+            title: "Special precautions",
+            details: null
+          },
+          {
+            title: "Possible side effects",
+            details: null
+          },
+          {
+            title: "Storage instructions",
+            details: null
+          },
+          {
+            title: "Emergency contact information",
+            details: null
+          }
+        ]
+      },
+      {
+        title: "Manufacturing and Quality Control",
+        substeps: [
+          {
+            title: "Manufacturing process details",
+            details: null
+          },
+          {
+            title: "Quality control procedures",
+            details: null
+          },
+          {
+            title: "Laboratory testing results",
+            details: null
+          },
+          {
+            title: "Compliance with good manufacturing practices (GMP)",
+            details: null
+          },
+          {
+            title: "Compliance with quality standards",
+            details: null
+          }
+        ]
+      },
+      {
+        title: "Regulatory Compliance",
+        substeps: [
+          {
+            title: "Compliance with Brazilian RDC 768 regulations",
+            details: null
+          },
+          {
+            title: "Compliance with ANVISA guidelines",
+            details: null
+          },
+          {
+            title: "Registration and licensing details",
+            details: null
+          }
+        ]
+      },
+      {
+        title: "Packaging Integrity",
+        substeps: [
+          {
+            title: "Package integrity testing results",
+            details: null
+          },
+          {
+            title: "Tamper-evident features",
+            details: null
+          },
+          {
+            title: "Packaging material suitability",
+            details: null
+          },
+          {
+            title: "Physical protection",
+            details: null
+          }
+        ]
+      },
+      {
+        title: "Additional Information",
+        substeps: [
+          {
+            title: "Product samples",
+            details: null
+          },
+          {
+            title: "Marketing materials",
+            details: null
+          },
+          {
+            title: "Other relevant documentation",
+            details: null
+          }
+        ]
+      },
+      {
+        title: "Checklist Completion",
+        substeps: [
+          {
+            title: "Verification of all necessary topics",
+            details: null
+          },
+          {
+            title: "Signatures of responsible parties",
+            details: null
+          },
+          {
+            title: "Date of completion",
+            details: null
+          },
+          {
+            title: "Note: This checklist is based on the information provided and may not include all the necessary items for compliance with Brazilian RDC 768. It is recommended to consult the official regulations for a comprehensive list.",
+            details: null
+          }
+        ]
+      }
+    ]
+  }
